442d28cd7d700fd178733d010c3b88fab1152442
Mozart Mafile'o <fetuu.mafileo@mds.ac.nz>
2021-11-09 14:04:26 +1300

    Flip Gravity Script

---
+++ GravityGame/Assets/Scripts/FlipG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipG : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+           _rb.gravityScale *= -1;
+        }
+    }
+}

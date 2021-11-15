efe12f6d95a6e128f6959dadb82100986cb23ed5
KokoOCE <iaramakomarea@gmail.com>
2021-11-15 14:09:17 +1300

    made level 1 besties

---
+++ GravityGame/Assets/Scripts/gunCollectLevel1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunCollectLevel1 : MonoBehaviour
+{
+    public GameObject arm;
+    bool armVisible;
+    // Start is called before the first frame update
+    void Start()
+    {
+        armVisible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (armVisible)
+        {
+            arm.SetActive(true);
+        }
+        if (!armVisible)
+        {
+            arm.SetActive(false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "arm")
+        {
+            armVisible = true;
+            collision.gameObject.SetActive(false);
+        }
+    }
+}

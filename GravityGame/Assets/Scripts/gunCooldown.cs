051f98476d238f3956fdb343b06ad8a71144cb5f
KokoOCE <iaramakomarea@gmail.com>
2021-11-16 13:39:55 +1300

    made the stasis gun work okay, still few errors. only issue is: -it doesnt move when its unfrozen but once you hit gravity flip again, it moves -if you flip gravity while its frozen, the gravity of the object may be opposite of other objects

---
+++ GravityGame/Assets/Scripts/gunCooldown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunCooldown : MonoBehaviour
+{
+    public bool canUseGunHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canUseGunHead = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

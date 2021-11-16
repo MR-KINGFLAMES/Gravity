bda9908f646d08443662e2bed6ddc0711057999d
KokoOCE <iaramakomarea@gmail.com>
2021-11-16 17:30:14 +1300

    level 6_V2 was added, so is keytesting scene

--- GravityGame/Assets/Scripts/CircularMovement.cs
+++ GravityGame/Assets/Scripts/CircularMovement.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CircularMovement : MonoBehaviour
 {
     public Vector2 Velocity = new Vector2(1, 0);
 
+    public gunCooldown canUseGun;
+    private bool isFrozen = false;
+    private bool canFreeze = false;
+    private bool countingDown = false;
+    private float timer = 5.0f;
+
     [Range(0, 5)]
-    public float RotateSpeed = 1f;
+    public float RotateSpeed = 5f;
     [Range(0, 5)]
     public float Radius = 1f;
 
@@ -15,22 +23,72 @@ public class CircularMovement : MonoBehaviour
     private void Start()
     {
         _centre = transform.position;
+        GameObject g = GameObject.FindGameObjectWithTag("manager");
+        canUseGun = g.GetComponent<gunCooldown>();
     }
 
     private void Update()
     {
-        _centre += Velocity * Time.deltaTime;
+        if (countingDown)
+        {
+            if (timer >= 0)
+            {
 
-        _angle += RotateSpeed * Time.deltaTime;
+                timer -= Time.deltaTime;
+            }
+            if (timer < 0)
+            {
+                Debug.Log("Time ran out");
+                timer = 5.0f;
 
-        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+                RotateSpeed = 5f;
+                canUseGun.canUseGunHead = true;
+                countingDown = false;
+                isFrozen = false;
+            }
+        }
+        if (!isFrozen)
+        {
+            _centre += Velocity * Time.deltaTime;
 
-        transform.position = _centre + offset;
+            _angle += RotateSpeed * Time.deltaTime;
+
+            var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+
+            transform.position = _centre + offset;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canUseGun.canUseGunHead == true)
+            {
+                Debug.Log("canuse gunhead is true it");
+                if (canFreeze)
+                {
+                    RotateSpeed = 1f;
+                    Debug.Log("You froze it");
+                    isFrozen = true;
+                    canUseGun.canUseGunHead = false;
+                    countingDown = true;
+                }
+            }
+        }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.DrawSphere(_centre, 0.1f);
         Gizmos.DrawLine(_centre, transform.position);
+    }
+    private void OnMouseEnter()
+    {
+        Debug.Log("You entered this object");
+        canFreeze = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("You left this object");
+        canFreeze = false;
     }
 }

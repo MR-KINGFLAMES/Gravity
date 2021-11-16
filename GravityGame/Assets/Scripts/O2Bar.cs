c592e3dd3fdb2e34423b0ceaa76f056f9962ab7a
Mozart Mafile'o <fetuu.mafileo@mds.ac.nz>
2021-11-16 15:13:32 +1300

    Health Bar / O2 Bar

---
+++ GravityGame/Assets/Scripts/O2Bar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class O2Bar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxO2(int O2)
+    {
+        slider.value = O2;
+        slider.maxValue = O2;
+
+       fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetO2(int O2)
+    {
+        slider.value = O2;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}

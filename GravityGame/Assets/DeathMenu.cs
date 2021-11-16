9c28834ac5fec89bcf938f1e2c5683b777f10535
Devantaye <92656221+Devantaye@users.noreply.github.com>
2021-11-17 11:52:02 +1300

    Added Pause Menu for each level - need to fix level 1 and 2

---
+++ GravityGame/Assets/DeathMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public static bool GameOver = false;
+    public GameObject GameOverUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Lost();
+        }
+        //else
+        //{
+        //    Winning();
+       // }
+    }
+
+    public void Lost()
+    {
+        GameOver = true;
+        GameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void Winning()
+    {
+        GameOver = false;
+        GameOverUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Back()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}

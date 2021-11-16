557010a28310dea54b14b69b0054722af05f1038
KokoOCE <iaramakomarea@gmail.com>
2021-11-17 00:05:12 +1300

    pls work bestie. i did level 4, just need to get o2

---
+++ GravityGame/Assets/Scripts/keySlot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keySlot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isInside = false;
+    public bool keySlotted = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isInside)
+            {
+                Debug.Log("You landed it inside");
+                keySlotted = true;
+                gameObject.SetActive(false);
+            }
+
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "slot")
+        {
+            Debug.Log("Its inside");
+            isInside = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "slot")
+        {
+            Debug.Log("Its no longer inside");
+            isInside = false;
+        }
+    }
+}
---
+++ GravityGame/Assets/Scripts/smashCrate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smashCrate : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject o2;
+    public GameObject tempKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key.SetActive(false);
+        o2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "heavyObject")
+        {
+            key.SetActive(true);
+            tempKey.SetActive(false);
+            o2.SetActive(true);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}

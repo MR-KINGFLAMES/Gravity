5be457f08a87ad48263f061bc567d05fef99b01e
KokoOCE <iaramakomarea@gmail.com>
2021-11-17 14:10:10 +1300

    level 6 completed

---
+++ GravityGame/Assets/Scripts/smashCrateLevel6.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smashCrateLevel6 : MonoBehaviour
+{
+    public GameObject o2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        o2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Saw")
+        {
+            o2.SetActive(true);
+            Destroy(gameObject);
+            col.gameObject.SetActive(false);
+
+        }
+    }
+}
+
